aa127e4fd20c2921ba4adfcff34a98489bdc7370
waaddaraghmeh0-sudo <waaddaraghmeh0@gmail.com>
2026-06-26 04:12:53 +0300

    initial commet with README

---
+++ loops/exercise01.cpp
@@ -0,0 +1,22 @@
+// loops - 01
+
+// Develop a C++ program that prints all numbers from 1 up to and including N using a for loop.
+// Given: int N = 10; 
+// Expected Output: Numbers from 1 to 10 are: 1 2 3 4 5 6 7 8 9 10
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int N = 10;
+
+for (int i=1; i<=N; i++) {
+    cout << i;
+}
+
+return 0;
+
+
+}
